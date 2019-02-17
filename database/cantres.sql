eaf57345a83de52b0be18cc64342fceacd86d47d
Wasym <wasymshykh@gmail.com>
2019-02-18 04:56:49 +0500

    Database file updated

--- database/cantres.sql
+++ database/cantres.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 07, 2019 at 01:59 AM
+-- Generation Time: Feb 18, 2019 at 04:55 AM
 -- Server version: 5.7.25-0ubuntu0.18.10.2
--- PHP Version: 7.3.1-1+ubuntu18.10.1+deb.sury.org+1
+-- PHP Version: 7.3.2-3+ubuntu18.10.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -37,6 +37,10 @@ CREATE TABLE `apartments` (
   `p_2` int(11) NOT NULL,
   `p_3` int(11) NOT NULL,
   `p_4` int(11) NOT NULL,
+  `p_1_a` int(11) DEFAULT '0',
+  `p_2_a` int(11) DEFAULT '0',
+  `p_3_a` int(11) DEFAULT '0',
+  `p_4_a` int(11) DEFAULT '0',
   `s_1_start` date NOT NULL,
   `s_1_end` date NOT NULL,
   `s_2_start` date NOT NULL,
@@ -46,14 +50,6 @@ CREATE TABLE `apartments` (
   `s_4_start` date NOT NULL,
   `s_4_end` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `apartments`
---
-
-INSERT INTO `apartments` (`id`, `name`, `desc_1`, `desc_2`, `adults`, `children`, `p_1`, `p_2`, `p_3`, `p_4`, `s_1_start`, `s_1_end`, `s_2_start`, `s_2_end`, `s_3_start`, `s_3_end`, `s_4_start`, `s_4_end`) VALUES
-(16, 'Can TerrA', 'Aparatment descripton line nu1', 'Television / Cocoina / Secador / 23m2 / Air Conditions', 5, 3, 30, 24, 50, 60, '2019-01-01', '2019-03-31', '2019-04-01', '2019-06-30', '2019-07-01', '2019-09-30', '2019-10-01', '2019-12-31'),
-(17, 'CAN MAR', 'CAN MAR descripton line nu1', 'Television / Cocoina / Secador / 23m2 / Air Conditions', 8, 5, 35, 55, 40, 70, '2019-01-01', '2019-03-31', '2019-04-01', '2019-06-30', '2019-08-01', '2019-10-31', '2019-10-01', '2019-12-31');
 
 -- --------------------------------------------------------
 
@@ -67,17 +63,6 @@ CREATE TABLE `apartment_images` (
   `apt_id` int(11) NOT NULL,
   `is_active` enum('1','0') DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `apartment_images`
---
-
-INSERT INTO `apartment_images` (`id`, `img_name`, `apt_id`, `is_active`) VALUES
-(26, '1547919276_slide_aire_1.jpg', 16, '1'),
-(27, '1547919279_slide_aire_2.jpg', 16, '1'),
-(28, '1547919283_slide_aire_3.jpg', 16, '1'),
-(29, '1548118857_slide_mar_6.jpg', 17, '1'),
-(30, '1548118860_slide_mar_4.jpg', 17, '1');
 
 -- --------------------------------------------------------
 
@@ -94,14 +79,6 @@ CREATE TABLE `cc_payments` (
   `cc_yy` int(11) NOT NULL,
   `res_no` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `cc_payments`
---
-
-INSERT INTO `cc_payments` (`id`, `cc_name`, `cc_number`, `cc_cvv`, `cc_mm`, `cc_yy`, `res_no`) VALUES
-(1, 'Wasym Shykh', '4454545454545454545', 245, 11, 22, 11),
-(3, 'Mujsdf Wasem', '45555555555445454', 554, 12, 20, 15);
 
 -- --------------------------------------------------------
 
@@ -120,20 +97,6 @@ CREATE TABLE `client` (
   `note` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `client`
---
-
-INSERT INTO `client` (`id`, `name`, `surname`, `email`, `phone`, `region`, `come_from`, `note`) VALUES
-(2, 'Waseem', 'Sheikh', 'test@test.com', '03201520120', 'Pakistan', 'Facebook', 'Hello World'),
-(3, 'Ahmed', 'Ali', 'testing@test.com', '03104520120', 'France', 'Instagram', 'Hello World How are you'),
-(4, 'Vikram', 'Kjil', 'kkjgldg@tetsgl.com', '03401520124', 'Itlay', 'Email', 'Hello World How are you'),
-(5, 'Ahmed', 'Sheikh', 'tablabajao@yandex.com', '03104520120', 'Pakistan', 'Facebook', 'Hello World'),
-(6, 'Ali', 'Sheikh', 'test@test.com', '03104520120', 'Pakistan', 'Email', 'hello worldz'),
-(17, 'Waseem', 'Sheikh', 'test@test.com', '03201520120', 'Pakistan', 'Other', 'Hello World'),
-(20, 'Muham', 'Wasm', 'testing@test.com', '454545454244', 'Itlay', 'Facebook', 'Nothing'),
-(21, 'Muham', 'Wasm', 'test@test.com', '454545454244', 'Itlay', 'Facebook', 'Nothing');
-
 -- --------------------------------------------------------
 
 --
@@ -150,7 +113,8 @@ CREATE TABLE `config` (
 --
 
 INSERT INTO `config` (`name`, `value`) VALUES
-('came_froms', 'Facebook, Instagram, Email List');
+('came_froms', 'Facebook, Instagram, Email List'),
+('payment_types', 'Credit Card, Paypal, Transferencia Bancaria');
 
 -- --------------------------------------------------------
 
@@ -171,19 +135,6 @@ CREATE TABLE `reserve` (
   `children` int(11) NOT NULL,
   `status` enum('0','1') NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `reserve`
---
-
-INSERT INTO `reserve` (`res_no`, `reserve_date`, `start_date`, `end_date`, `apt_id`, `client_id`, `cost`, `payment_via`, `adults`, `children`, `status`) VALUES
-(1, '2019-01-22', '2019-03-30', '2019-04-01', 16, 2, 84, 'paypal', 3, 2, '1'),
-(2, '2019-01-22', '2019-01-02', '2019-01-10', 16, 3, 270, 'paypal', 2, 3, '1'),
-(3, '2019-01-22', '2019-01-23', '2019-01-27', 16, 4, 150, 'Credit Card', 3, 1, '1'),
-(4, '2019-01-22', '2019-03-30', '2019-04-01', 17, 5, 125, 'paypal', 1, 3, '1'),
-(11, '2019-01-23', '2019-01-24', '2019-01-26', 17, 17, 116, 'Credit Card', 1, 2, '0'),
-(14, '2019-02-06', '2019-02-07', '2019-02-20', 16, 20, 462, 'paypal', 2, 3, '0'),
-(15, '2019-02-07', '2019-02-15', '2019-02-20', 17, 21, 210, 'Credit Card', 6, 3, '1');
 
 --
 -- Indexes for dumped tables
@@ -227,12 +178,12 @@ ALTER TABLE `reserve`
 -- AUTO_INCREMENT for table `apartments`
 --
 ALTER TABLE `apartments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 --
 -- AUTO_INCREMENT for table `apartment_images`
 --
 ALTER TABLE `apartment_images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 --
 -- AUTO_INCREMENT for table `cc_payments`
 --
@@ -242,12 +193,12 @@ ALTER TABLE `cc_payments`
 -- AUTO_INCREMENT for table `client`
 --
 ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 --
 -- AUTO_INCREMENT for table `reserve`
 --
 ALTER TABLE `reserve`
-  MODIFY `res_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `res_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
